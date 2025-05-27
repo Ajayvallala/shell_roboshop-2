c78285b89cbb708c626372feffeabd140f8f7599
ajay <vallala1602@gmail.com>
2025-05-27 14:56:05 +0530

    All

--- payment.sh
+++ payment.sh
@@ -5,11 +5,12 @@ app_name="payment"
 source ./common.sh
 
 checkroot
+
 app_setup
 
-dnf install python3 gcc python3-devel -y
+dnf install python3 gcc python3-devel -y &>>$LOG_FILE
 
-pip3 install -r requirements.txt
+pip3 install -r requirements.txt &>>$LOG_FILE
 
 appservice_setup
 
