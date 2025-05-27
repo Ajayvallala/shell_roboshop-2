835d0c0a44cfe757cba83578664450f38fd23345
ajay <vallala1602@gmail.com>
2025-05-27 14:54:16 +0530

    All

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -5,7 +5,7 @@ source ./common.sh
 checkroot
 
 echo "Please enter rabbitmq user password"
-read Rabbirmq_user_password
+read -s Rabbirmq_user_password
 
 cp $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
 VALIDATE $? "Creating rabbitmq repo file"
@@ -19,7 +19,7 @@ VALIDATE $? "Enabling rabbitmq"
 systemctl start rabbitmq-server &>>$LOG_FILE
 VALIDATE $? "Starting rabbitmq"
 
-rabbirmqctl add user roboshop $Rabbirmq_user_password &>>$LOG_FILE
+rabbirmqctl add_user roboshop $Rabbirmq_user_password &>>$LOG_FILE
 VALIDATE $? "Creating rabbitmq user"
 
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOG_FILE
